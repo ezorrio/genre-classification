67aead650ae8596cca9973dd5f79b6fd76b8ccf5
ezorrio <ezorrio@gmail.com>
2021-03-31 15:54:33 +0200

    Added experiments folder. Path fixes

--- generate_R_alternative.py
+++ generate_R_alternative.py
@@ -2,17 +2,19 @@
 
 import numpy as np
 
+
 def generate_R(rows, columns):
     """ Generates sparse zeros-matrix with 1/6-th having -sqrt(3) and
     1/6-th having sqrt(3). """
-    
-    R = np.zeros((rows*columns))
-    
+
+    R = np.zeros((rows * columns))
+
     # 1/6 of length of matrix
-    j = int(np.round(rows*columns / 6))
-    
+    j = int(np.round(rows * columns / 6))
+
     # overwrites 1/6-th of array with sqrt(3) and 1/6-th with -sqrt(3)
-    R[:j] = np.sqrt(3); R[j:2*j] = -np.sqrt(3)
+    R[:j] = np.sqrt(3)
+    R[j:2 * j] = -np.sqrt(3)
     np.random.shuffle(R)
-    
+
     return np.reshape(R, (rows, columns))
--- genre_classification_justus.py
+++ genre_classification_justus.py
@@ -1,28 +1,24 @@
 import pandas as pd
 import numpy as np
 
-
-
 '''
 Importing and Splitting the Data
 '''
 
-df_tracks = pd.read_csv('fma_metadata/tracks.csv', index_col=0, header=[0, 1])
+df_tracks = pd.read_csv('metadata/tracks.csv', index_col=0, header=[0, 1])
 df_tracks = df_tracks[df_tracks['set']['subset'] == 'small']
-df_features = pd.read_csv('fma_metadata/features.csv', index_col=0, header=[0, 1])
-
+df_features = pd.read_csv('metadata/features.csv', index_col=0, header=[0, 1])
 
 train_tracks = df_tracks.loc[(df_tracks['set']['split'] == 'training')]
 test_tracks = df_tracks.loc[(df_tracks['set']['split'] == 'test')]
 validation_tracks = df_tracks.loc[(df_tracks['set']['split'] == 'validation')]
 
-
 '''
 Generate Random Matrix
 '''
 
-def gen_rnd_matrix(rows, cols):
 
+def gen_rnd_matrix(rows, cols):
     matrix = np.empty((rows, cols))
     print(matrix)
 
@@ -31,15 +27,12 @@ def gen_rnd_matrix(rows, cols):
 
             rng = np.random.rand()
             print(rng)
-            
-            if rng < 1/6:
+
+            if rng < 1 / 6:
                 matrix[i, j] = np.sqrt(3)
-            
-            elif rng >= 1/6 and rng < 5/6:
+            elif 1 / 6 <= rng < 5 / 6:
                 matrix[i, j] = 0
-            
             else:
                 matrix[i, j] = -1 * np.sqrt(3)
 
     return matrix
-
