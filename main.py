56704553f757ac90e0c77cd3b88f1720ec49bb14
glarkstoat <74681570+glarkstoat@users.noreply.github.com>
2021-04-13 16:03:34 +0200

    runtime-measure added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from MusicSearch_new import *
+import datetime
 
 if __name__ == '__main__':
 
@@ -9,17 +10,26 @@ if __name__ == '__main__':
         
     search = MusicSearch("metadata/", number_of_hashtables, hash_length)
     
-    # Phase-1: Training data vs. validation set 
+    """ Phase-1: Training data vs. validation set """
     training_data = search.data.get_training_data()
     validation_data = search.data.get_validation_data() 
     
-    search.train(training_data)
-    search.test(validation_data)
-
     print('-----------------------------------------')
     print('Genre classification for training and validation data:\n')
     
-    # Phase-2: Training data & validation set vs. test data
+    search.train(training_data)
+
+    start = datetime.datetime.now()
+    search.test(validation_data)
+    
+    # Prints the runtime of the given subtask
+    runtime = (datetime.datetime.now() - start).total_seconds()  
+    print(f'Runtime: {runtime/60:.2f} minutes.\n')
+    
+    
+    """ Phase-2: Training data & validation set vs. test data """
+    """ NEW INSTANCE OF MusicSearch NEEDED? """
+
     training_data = search.data.get_training_data()
     validation_data = search.data.get_validation_data()
     
@@ -28,9 +38,15 @@ if __name__ == '__main__':
     
     test_data = search.data.get_test_data() 
     
-    search.train(training_data)
-    search.test(test_data)
-
     print('-----------------------------------------')
     print('Genre classification for training and validation data combined'
           ' vs. test data\n')
+    
+    search.train(training_data)
+    
+    start = datetime.datetime.now()
+    search.test(test_data)
+
+    # Prints the runtime of the given subtask
+    runtime = (datetime.datetime.now() - start).total_seconds()  
+    print(f'\nRuntime: {runtime/60:.2f} minutes.\n')
